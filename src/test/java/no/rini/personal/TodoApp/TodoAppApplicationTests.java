e74ff5b8eb7e397bc09f7bce47a92bbb87713448
Kiran <skirankumars31@gmail.com>
2020-03-08 21:20:48 +0100

    Implemented github actions for push request

--- src/test/java/no/rini/personal/TodoApp/TodoAppApplicationTests.java
+++ src/test/java/no/rini/personal/TodoApp/TodoAppApplicationTests.java
@@ -1,7 +1,9 @@
 package no.rini.personal.TodoApp;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class TodoAppApplicationTests {
@@ -9,5 +11,17 @@ class TodoAppApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void passTest() {
+		assertEquals(true,true);
+	}
+
+	@Test
+	void failTest() {
+		assertEquals(true,false);
+	}
+
+
 
 }
