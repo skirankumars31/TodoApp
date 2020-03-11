73d4af4523d1fa9972a6344eeea90644231a934c
Kiran <skirankumars31@gmail.com>
2020-03-11 23:48:11 +0100

    Added the helloWorld Api (#3)

---
+++ src/test/java/no/rini/personal/TodoApp/api/HelloWorldControllerTest.java
@@ -0,0 +1,26 @@
+package no.rini.personal.TodoApp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloWorldControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void sayHello_should_return_correct_string() {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/sayHello/Kiran",
+                String.class)).contains("Hello Kiran");
+    }
+}
