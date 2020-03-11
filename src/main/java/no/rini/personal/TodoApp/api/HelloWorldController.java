27e3eb51f8d2189a9d205c163905422d652a5442
Kiran <skirankumars31@gmail.com>
2020-03-11 23:45:55 +0100

    Added the helloWorld Api

---
+++ src/main/java/no/rini/personal/TodoApp/api/HelloWorldController.java
@@ -0,0 +1,14 @@
+package no.rini.personal.TodoApp.api;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorldController {
+
+    @GetMapping(value = "/sayHello/{name}", produces = "application/json")
+    public @ResponseBody String sayHello(@PathVariable("name") String name) {
+        return "Hello " + name;
+    }
+
+}
