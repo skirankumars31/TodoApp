22a0d097ad787dca6c1101e7087d4a9ea6ac6418
Kiran <skirankumars31@gmail.com>
2020-03-13 14:47:11 +0100

    Implemented a simple post mapping

--- src/main/java/no/rini/personal/TodoApp/api/HelloWorldController.java
+++ src/main/java/no/rini/personal/TodoApp/api/HelloWorldController.java
@@ -6,10 +6,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class HelloWorldController {
 
-    @GetMapping(value = "/sayHello/{name}", produces = "application/json")
+    @GetMapping(value = "/sayHello/{name}",produces = "application/json")
     public @ResponseBody
     String sayHello(@PathVariable("name") String name) {
         return "Hello " + name;
     }
 
+    @PostMapping(value = "/addTask",produces = "application/json",consumes = "text/plain")
+    public @ResponseBody String addTask(@RequestBody String name) {
+        return "The name in the post request is "+name;
+    }
 }
