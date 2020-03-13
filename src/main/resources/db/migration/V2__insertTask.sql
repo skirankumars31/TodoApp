573abe46e2713e3ff90c2ad2ab8ffbf1ebc60275
Kiran <skirankumars31@gmail.com>
2020-03-13 13:30:01 +0100

    fixed bug 9

---
+++ src/main/resources/db/migration/V2__insertTask.sql
@@ -0,0 +1,3 @@
+INSERT INTO task(id,title,description) VALUES (1, 'Learn Java','Complete a Project');
+INSERT INTO task(id,title,description) VALUES (2, 'Wash home','Clean with mop');
+INSERT INTO task(id,title,description) VALUES (3, 'Cook food','Make Pizza');
