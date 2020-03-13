aa94a5af500a83d6a6db1a4a3534708f1a79d0ee
Kiran <skirankumars31@gmail.com>
2020-03-13 14:27:12 +0100

    Implemented spring profile (#12)

---
+++ src/main/resources/db/migration/test/V2__insertTask.sql
@@ -0,0 +1,3 @@
+INSERT INTO task(id,title,description) VALUES (1, 'Test - Learn Java','Complete a Project');
+INSERT INTO task(id,title,description) VALUES (2, 'Test - Wash home','Clean with mop');
+INSERT INTO task(id,title,description) VALUES (3, 'Test - Cook food','Make Pizza');
