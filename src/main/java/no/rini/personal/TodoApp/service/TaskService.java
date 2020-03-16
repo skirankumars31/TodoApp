fb0280c44a50d2ad39844799c10636331185304b
Kiran <skirankumars31@gmail.com>
2020-03-16 21:51:38 +0100

    Added logging to the project using slf4j and log4j2

--- src/main/java/no/rini/personal/TodoApp/service/TaskService.java
+++ src/main/java/no/rini/personal/TodoApp/service/TaskService.java
@@ -2,6 +2,8 @@ package no.rini.personal.TodoApp.service;
 
 import no.rini.personal.TodoApp.model.Task;
 import no.rini.personal.TodoApp.repository.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,34 +17,41 @@ public class TaskService {
     @Autowired
     private TaskRepository taskRepository;
 
+    Logger logger = LoggerFactory.getLogger(TaskService.class);
+
     ArrayList<Task> tasks = new ArrayList<>();
 
     public List<Task> getallTasks() {
+        logger.info("Invoked the getallTasks in Service");
         return taskRepository.findAll();
     }
 
     public Task getTask(Integer id) {
+        logger.info("Invoked the getTask in Service");
         Optional<Task> foundTask = taskRepository.findById(id);
         return foundTask.orElse(null);
     }
 
     public String addTask(Task task) {
+        logger.info("Invoked the addTask in Service");
         taskRepository.saveAndFlush(task);
         return "Task Added";
     }
 
     public String editTask(Task editTask, Integer id) {
 
-        if(taskRepository.existsById(id)){
+        logger.info("Invoked the editTask in Service");
+
+        if (taskRepository.existsById(id)) {
             taskRepository.deleteById(id);
             taskRepository.saveAndFlush(editTask);
             return "Task edited";
-        }
-        else
+        } else
             return "Task does not exist";
     }
 
     public String deleteTask(Integer id) {
+        logger.info("Invoked the deleteTask in Service");
         taskRepository.deleteById(id);
         return "Task deleted";
     }
