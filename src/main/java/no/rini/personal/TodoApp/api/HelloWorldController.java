37ab0a449a29fc6d7fa64ef13cda06f50d9e057e
Kiran <skirankumars31@gmail.com>
2020-03-16 23:36:41 +0100

    Added logging to the project using slf4j and log4j2 (#17)
    * Added logging to the project using slf4j and log4j2

--- src/main/java/no/rini/personal/TodoApp/api/HelloWorldController.java
+++ src/main/java/no/rini/personal/TodoApp/api/HelloWorldController.java
@@ -2,12 +2,13 @@ package no.rini.personal.TodoApp.api;
 
 import no.rini.personal.TodoApp.model.Task;
 import no.rini.personal.TodoApp.service.TaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api")
@@ -16,38 +17,44 @@ public class HelloWorldController {
     @Autowired
     TaskService taskService;
 
-    ArrayList<Task> tasks = new ArrayList<>();
+    Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 
     @GetMapping(value = "/sayHello/{name}", produces = "application/json")
     public ResponseEntity<String> sayHello(@PathVariable("name") String name) {
+        logger.info("Invoked sayHello Method");
         return ResponseEntity.ok("Hello " + name);
     }
 
     @GetMapping(value = "/getallTasks", produces = "application/json")
     public ResponseEntity<?> getallTasks() {
+        logger.info("Invoked getallTasks Method");
         return ResponseEntity.ok(taskService.getallTasks());
     }
 
     @GetMapping(value = "/getTask/{id}", produces = "application/json")
     public ResponseEntity<?> getTask(@PathVariable("id") Integer id) {
+        logger.info("Invoked getTask Method");
         return ResponseEntity.ok(taskService.getTask(id));
     }
 
     @PostMapping(value = "/addTask", produces = "application/json", consumes = "application/json")
     public ResponseEntity<String> addTask(@RequestBody Task task) {
+        logger.info("Invoked addTask Method");
         return ResponseEntity.ok(taskService.addTask(task));
     }
 
     @PutMapping("/editTask/{id}")
     public ResponseEntity<?> editTask(@Valid @RequestBody Task editTask, @PathVariable("id") Integer id) {
-        tasks.removeIf((Task task) -> editTask.getId().equals(id));
-        tasks.add(editTask);
-        return ResponseEntity.ok(taskService.editTask(editTask,id));
+        logger.info("Invoked editTask Method");
+        taskService.getallTasks().removeIf((Task task) -> editTask.getId().equals(id));
+        taskService.addTask(editTask);
+        return ResponseEntity.ok(taskService.editTask(editTask, id));
     }
 
     @DeleteMapping("/deleteTask/{id}")
     public ResponseEntity<?> deleteTask(@PathVariable("id") Integer id) {
-        tasks.removeIf((task) -> (task.getId().equals(id)));
+        logger.info("Invoked deleteTask Method");
+        taskService.getallTasks().removeIf(task -> (task.getId().equals(id)));
         return ResponseEntity.ok(taskService.deleteTask(id));
     }
 
